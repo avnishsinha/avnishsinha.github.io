c016b2ffa9c6f6ca22e7a09d9d41f6768ff4de00
avnishsinha <93894240+avnishsinha@users.noreply.github.com>
2023-08-15 21:07:29 -0700

    Icons and tabs
    Changed the icons and did some changes with the tabs.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
 const navMenu = document.querySelector(".nav-menu");
 
 navMenu.addEventListener("click", (e) => {
-  e.preventDefault();
-
   const target = e.target.getAttribute("href");
-  const section = document.querySelector(target);
 
-  if (section) { // Add a check to ensure section exists
-    const sectionTop = section.offsetTop - 80;
+  // Only handle internal links
+  if (target && target.startsWith("#")) {
+    e.preventDefault();
+    const section = document.querySelector(target);
 
-    window.scrollTo({
-      top: sectionTop,
-      behavior: "smooth",
-    });
+    if (section) { // Add a check to ensure section exists
+      const sectionTop = section.offsetTop - 80;
+
+      window.scrollTo({
+        top: sectionTop,
+        behavior: "smooth",
+      });
+    }
   }
 });
-
-
-function toggleMenu() {
-  const navMenu = document.getElementById('nav-menu');
-  navMenu.classList.toggle('collapsed');
-}
